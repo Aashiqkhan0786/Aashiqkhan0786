59ac313c7f88dcd2647b2ac7054b07da2bd3d956
AashiqKhan <aashiqkhan332@gmail.com>
2023-06-02 13:54:45 +0530

    html files

---
+++ Desktop/NineMd-Html/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  var owl = $('.owl-carousel');
+  owl.owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    items: 1,
+  });
+  
+});
---
+++ README.md
@@ -0,0 +1,1 @@
+# Aashiqkhan0786
